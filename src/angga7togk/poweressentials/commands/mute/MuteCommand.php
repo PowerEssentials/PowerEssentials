257ccf5b953727ba8458cc7b7111d0d19de275f6
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2025-02-02 19:19:36 +0700

    Update MuteCommand.php

--- src/angga7togk/poweressentials/commands/mute/MuteCommand.php
+++ src/angga7togk/poweressentials/commands/mute/MuteCommand.php
@@ -1,16 +1,31 @@
 <?php
 
-namespace angga7togk\poweressentials\commands\mute;
+/*
+ *   ____                        _____                    _   _       _
+ *  |  _ \ _____      _____ _ __| ____|___ ___  ___ _ __ | |_(_) __ _| |___
+ *  | |_) / _ \ \ /\ / / _ \ '__|  _| / __/ __|/ _ \ '_ \| __| |/ _` | / __|
+ *  |  __/ (_) \ V  V /  __/ |  | |___\__ \__ \  __/ | | | |_| | (_| | \__ \
+ *  |_|   \___/ \_/\_/ \___|_|  |_____|___/___/\___|_| |_|\__|_|\__,_|_|___/
+ *
+ *
+ * This file is part of PowerEssentials plugins.
+ *
+ * (c) Angga7Togk <kiplihode123321@gmail.com>
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
-use angga7togk\poweressentials\config\PEConfig;
+namespace angga7togk\poweressentials\commands;
+
 use angga7togk\poweressentials\i18n\PELang;
 use angga7togk\poweressentials\PowerEssentials;
-use angga7togk\poweressentials\managers\UserManager;
 use pocketmine\command\CommandSender;
-use pocketmine\player\Player;
 
-class MuteCommand extends PECommand
+class MuteCommand extends PECommand implements PluginOwned
 {
+    use PluginOwnedTrait;
+
     public function __construct()
     {
         parent::__construct("mute", "Mute a player", "/mute <player> [reason]", []);
@@ -33,7 +48,6 @@ class MuteCommand extends PECommand
         $playerName = array_shift($args);
         $reason = empty($args) ? $lang->translateString('mute.default_reason') : implode(" ", $args);
 
-        $player = PowerEssentials::getInstance()->getServer()->getPlayerByPrefix($playerName);
         $userManager = PowerEssentials::getInstance()->getUserManager();
 
         if ($userManager->isMuted($playerName)) {
@@ -43,9 +57,5 @@ class MuteCommand extends PECommand
 
         $userManager->mutePlayer($playerName, $reason);
         $sender->sendMessage($prefix . $lang->translateString('mute.success', [$playerName, $reason]));
-
-        if ($player instanceof Player) {
-            $player->sendMessage($prefix . $lang->translateString('mute.notify', [$reason]));
-        }
     }
 }
