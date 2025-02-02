1555cad3cc6076104d7c3fdd9345f34e827725b4
AzliRynz <135945845+AzliRynz@users.noreply.github.com>
2025-02-02 19:14:22 +0700

    Update MuteTrait.php

--- src/angga7togk/poweressentials/manager/user/MuteTrait.php
+++ src/angga7togk/poweressentials/manager/user/MuteTrait.php
@@ -1,38 +1,52 @@
 <?php
 
+/*
+ *   ____                        _____                    _   _       _
+ *  |  _ \ _____      _____ _ __| ____|___ ___  ___ _ __ | |_(_) __ _| |___
+ *  | |_) / _ \ \ /\ / / _ \ '__|  _| / __/ __|/ _ \ '_ \| __| |/ _` | / __|
+ *  |  __/ (_) \ V  V /  __/ |  | |___\__ \__ \  __/ | | | |_| | (_| | \__ \
+ *  |_|   \___/ \_/\_/ \___|_|  |_____|___/___/\___|_| |_|\__|_|\__,_|_|___/
+ *
+ *
+ * This file is part of PowerEssentials plugins.
+ *
+ * (c) Angga7Togk <kiplihode123321@gmail.com>
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 namespace angga7togk\poweressentials\manager\user;
+
+use angga7togk\poweressentials\manager\UserManager;
 
 trait MuteTrait
 {
-    private array $mutes = [];
+    public function isMuted(string $playerName): bool
+    {
+        /** @var UserManager $this */
+        return isset($this->getData()->get('mutes')[$playerName]);
+    }
 
     public function mutePlayer(string $playerName, string $reason): void
     {
-        $this->mutes[$playerName] = $reason;
-        $this->saveMuteList();
+        $this->getData()->setNested("mutes.$playerName", $reason);
+        $this->getData()->save();
     }
 
     public function unmutePlayer(string $playerName): void
     {
-        unset($this->mutes[$playerName]);
-        $this->saveMuteList();
+        $this->getData()->removeNested("mutes.$playerName");
+        $this->getData()->save();
     }
 
-    public function isMuted(string $playerName): bool
+    public function getMuteReason(string $playerName): ?string
     {
-        return isset($this->mutes[$playerName]);
+        return $this->getData()->getNested("mutes.$playerName", null);
     }
 
-    private function saveMuteList(): void
+    public function getMutedPlayers(): array
     {
-        $config = PowerEssentials::getInstance()->getConfig();
-        $config->set("muted_players", $this->mutes);
-        $config->save();
-    }
-
-    public function loadMuteList(): void
-    {
-        $config = PowerEssentials::getInstance()->getConfig();
-        $this->mutes = $config->get("muted_players", []);
+        return array_keys($this->getData()->get('mutes', []));
     }
 }
