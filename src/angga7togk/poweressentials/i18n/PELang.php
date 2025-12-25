dfc69bb4a7c8e93b69f6a7f8557eff7a9642f77f
Aaa <wortelsmpofficial@gmail.com>
2025-12-25 20:41:26 +0700

    fix: resolve startup crash and PHPStan errors

--- src/angga7togk/poweressentials/i18n/PELang.php
+++ src/angga7togk/poweressentials/i18n/PELang.php
@@ -43,7 +43,7 @@ class PELang extends Language
 
     public function translateString(string $str, array $params = [], ?string $onlyPrefix = null, int &$untranslatedParameterCount = 0): string
     {
-        $result = parent::translateString($str, $params, $onlyPrefix, int &$untranslatedParameterCount = 0);
+        $result = parent::translateString($str, $params, $onlyPrefix, $untranslatedParameterCount);
         if (stripos(trim($str), 'error') !== false) {
             return TextFormat::RED . $result;
         }
